badc12b7429e79c9b05439b744c07da4b72fdae0
dpapoutsis <dimitris.papoutsis@sword-group.com>
2023-05-16 00:59:47 +0300

    smart contract  function implementation

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,15 +2,27 @@ const { writeToFolder } = require("./utils");
 const { ethers } = require("hardhat");
 
 const contract = {
-  contractName: "MyNFT",
-  tokenName: "amazing name",
-  tokenSymbol: "ANFT",
+  contractName: "AwsomeNFT",
+  tokenName: "Famous Paintings",
+  tokenSymbol: "FPNT",
+  tokenURI: "https://ipfs.io/ipfs/QmXWTYxHaEeVPrXa7FoC5sQzdnyBTtDoziTPtgmrceXrCf/",
+  totalNfts: 10,
 };
 
-async function main({ contractName, tokenName, tokenSymbol }) {
-  const contract_name = contractName;
-  const Contract = await ethers.getContractFactory(contract_name);
-  const contract = await Contract.deploy(tokenName, tokenSymbol);
+async function main({
+  contractName,
+  tokenName,
+  tokenSymbol,
+  tokenURI,
+  totalNfts,
+}) {
+  const Contract = await ethers.getContractFactory(contractName);
+  const contract = await Contract.deploy(
+    tokenName,
+    tokenSymbol,
+    tokenURI,
+    totalNfts
+  );
   await contract.deployed();
 
   await writeToFolder(
